355819df9c6a7d2bc963083021dd6efd65ac5b85
VKush <Vkushniruk1@gmail.com>
2017-11-13 16:39:08 +0200

    new template

---
+++ template/js/script.js
@@ -0,0 +1,21 @@
+    $(function() {
+      $("#owl-demo").owlCarousel({
+
+      navigation : true,
+      slideSpeed : 400,
+      paginationSpeed : 400,
+      singleItem : true,
+      rewindSpeed: 900,
+      autoPlay: false,
+      stopOnHover : true
+      });
+      $("#owl-demo1").owlCarousel({
+
+      navigation : false,
+      slideSpeed : 30,
+      paginationSpeed : 40,
+      singleItem : true,
+      rewindSpeed: 300,
+      autoPlay: false
+      });
+    });
