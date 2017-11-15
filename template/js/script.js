2579c1912fd7d7463ccc83a7f69eeb8be75a212f
VKush <Vkushniruk1@gmail.com>
2017-11-15 21:37:10 +0200

    15/11/2017

--- template/js/script.js
+++ template/js/script.js
@@ -1,41 +1,20 @@
-    $(function() {
-    var images = [
-        'img/changed/product1.png',
-        'img/changed/product2.png',
-        'img/changed/product3.png',
-        'img/changed/product4.png',
-        'img/changed/product5.png',
-        'img/changed/product6.png',
-        'img/changed/product7.png',
-        'img/changed/product8.png',
-        'img/changed/product9.png',
-        'img/changed/product10.png'
-    ]
-            $(".changeback").mouseenter(function() {
-        var newUrl = 'url("img/changed/product' +$(this).css("background-image").split("product")[1];
-        $(this).css("background-image", newUrl).css("background-size", "100% 100%");
-})
-    $(".changeback").mouseleave(function() {
-        var newUrl = 'url("img/product' +$(this).css("background-image").split("product")[1];
-        $(this).css("background-image", newUrl).css("background-size", "100% 100%");
-})
-      $("#owl-demo").owlCarousel({
+$(function () {
+    $("#owl-demo").owlCarousel({
 
-      navigation : true,
-      slideSpeed : 400,
-      paginationSpeed : 400,
-      singleItem : true,
-      rewindSpeed: 900,
-      autoPlay: false,
-      stopOnHover : true
-      });
-      $("#owl-demo1").owlCarousel({
-
-      navigation : false,
-      slideSpeed : 30,
-      paginationSpeed : 40,
-      singleItem : true,
-      rewindSpeed: 300,
-      autoPlay: false
-      });
+        navigation : true,
+        slideSpeed : 400,
+        paginationSpeed : 400,
+        singleItem : true,
+        rewindSpeed: 900,
+        autoPlay: false,
+        stopOnHover : true
     });
+    $("#owl-demo1").owlCarousel({
+        navigation : false,
+        slideSpeed : 30,
+        paginationSpeed : 40,
+        singleItem : true,
+        rewindSpeed: 300,
+        autoPlay: false
+    });
+});
