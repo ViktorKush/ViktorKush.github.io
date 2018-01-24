b51d90d19e85d1895daf0c87dc2a2ea3fdd7cf62
VKush <Vkushniruk1@gmail.com>
2018-01-24 19:35:44 +0200

    deleted some old sfuff

--- galian/form reka/jquery.js
+++
@@ -1,8 +0,0 @@
-﻿$(function() {
-  $("a[rel]").overlay(function() {
-  var wrap = this.getContent().find("div.wrap");
-  if (wrap.is(":empty")) {
-  wrap.load(this.getTrigger().attr("href"));
-  }
-  });
-  });
